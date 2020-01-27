41d3acadbbcab2cca10da762592bf67e3177ed80
Lucas Sherwin <lksherwin@gmail.com>
2020-01-27 21:40:53 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
@@ -7,5 +8,7 @@ def pretty_print_nds(nds)
   pp nds
 end
 
-def print_first_directors_movie_titles
+def print_first_directors_movie_titles(nds)
+  return 
+  
 end
